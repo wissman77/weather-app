2689932023acd50bfeea3ae1cbb8eb6e4fbbe64d
Wissam Mansour <wissman77@gmail.com>
2023-02-26 10:53:10 +0200

    add getWeatherDataFor5Days and getData

--- src/modules/apiFuncs.js
+++ src/modules/apiFuncs.js
@@ -1,13 +1,26 @@
 const apiKey = 'e23f4e2a8fd980f3490ac76932aae2d0';
 
-async function getCurrentWeatherDataByCity(cityName, units = 'metric') {
-  const url = new URL('https://api.openweathermap.org/data/2.5/weather');
+async function getData(requestedUrl, cityName, units) {
+  const url = new URL(requestedUrl);
   url.searchParams.set('q', cityName);
   url.searchParams.set('units', units);
   url.searchParams.set('appid', apiKey);
 
-  const response = await fetch(url, { mode: 'cors' });
-  const data = await response.json();
+  try {
+    const response = await fetch(url, { mode: 'cors' });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getCurrentWeatherDataByCity(cityName, units = 'metric') {
+  const data = await getData(
+    'https://api.openweathermap.org/data/2.5/weather',
+    cityName,
+    units
+  );
 
   const currentData = {
     cityName: data.name,
@@ -18,8 +31,17 @@ async function getCurrentWeatherDataByCity(cityName, units = 'metric') {
     humidity: data.main.humidity,
     windSpeed: data.wind.speed,
   };
+
+  console.log(data);
+  return currentData;
 }
 
-async function getWeatherDataFor5Days(cityName, units = 'metric') {}
+async function getWeatherDataFor5Days(cityName, units = 'metric') {
+  const data = getData(
+    'https://api.openweathermap.org/data/2.5/weather',
+    cityName,
+    units
+  );
+}
 
 export { getCurrentWeatherDataByCity, getWeatherDataFor5Days };
